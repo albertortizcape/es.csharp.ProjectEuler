2f2ef16358ff6be001a1fb7f3efad8d7992a630d
Alberto Ortiz Capellán <albertortizcape@outlook.com>
2016-06-20 09:21:02 +0200

    Change the first comment. Add the Url to the problem.

--- Problem3/Problem3/Program.cs
+++ Problem3/Problem3/Program.cs
@@ -11,10 +11,13 @@ namespace Problem3
         static void Main(string[] args)
         {
             /*
-                The prime factors of 13195 are 5, 7, 13 and 29.
+				https://projecteuler.net/problem=3
+			
+				The prime factors of 13195 are 5, 7, 13 and 29.
 
-                What is the largest prime factor of the number 600851475143 ?
-            */
+				What is the largest prime factor of the number 600851475143 ?
+			
+			*/
 
             // First, start with the lowest factors and check if the biggest quotients are primes
             NumberOperationsController controller = new NumberOperationsController();
